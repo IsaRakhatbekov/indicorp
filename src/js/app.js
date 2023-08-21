412dff7479647c5c19000f2d0c6c99d1668494aa
IsaRakhatbekov <isarahatbekov250@gmail.com>
2023-08-21 19:05:30 +0600

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from "./modules/functions.js";
+
+//========================================= another scripts ===========================================================
+import script from "./files/script.js";
+//========================================= another scripts ===========================================================
+
+flsFunctions.inWebp();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
